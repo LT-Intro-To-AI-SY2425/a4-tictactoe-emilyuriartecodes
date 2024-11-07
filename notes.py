af13142e70b52f42435006460ca5e5b4db57f5ab
Your Name <euriarte1@cps.edu>
2024-11-07 11:33:49 -0600

    done

--- notes.py
+++ notes.py
@@ -2,3 +2,18 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    def __init__(self, n="No Name", a=0):
+        self.name = n
+        self.age = a
+    def __str__(self) -> str:
+        s = f"{self.name} is {self.age} years old"
+        return s
+
+logan = Dog("Logan", 8)
+cookie = Dog("Cookie", 8)
+maple= Dog("Maple", 1)
+
+print(logan.name)
+print(cookie)
+print(maple)
